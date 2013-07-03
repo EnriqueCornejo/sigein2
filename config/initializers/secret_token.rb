e8b3ef853a8c8e1a5f5066f16fd9f2fc708a4888
Enrique Cornejo <enrique@cornejo.me>
2013-07-03 10:35:17 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sigein2::Application.config.secret_token = '7eb2ae7f037a091ad9c33ff7eaaf3c7fda72a4726c8933646251c6afe1708764f98b6d239b0694af14274101ad44328be99873d9b4ecc9e7832f1b01088dd7d7'
